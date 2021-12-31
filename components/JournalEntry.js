f5ab54dd3b7ab55ba75050ac4755eb70d115e068
rburgos80 <rburgos80@gmail.com>
2021-12-30 19:15:13 -0500

    Added episode carousel

--- components/JournalEntry.js
+++ components/JournalEntry.js
@@ -9,19 +9,27 @@ import {
   Dropdown,
   DropdownButton,
 } from "react-bootstrap";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const JournalEntry = ({ show, openDefault = false }) => {
   const [open, setOpen] = useState(openDefault);
-  const [displaySeason, setDisplaySeason] = useState(1);
-  const seasons = [1, 2, 3, 4, 5, 6];
-  const episodes = [
-    "episode 1",
-    "episode 2",
-    "episode 3",
-    "episode 4",
-    "episode 5",
-  ];
+  const [displaySeason, setDisplaySeason] = useState("1");
+  const [displayEpisode, setDisplayEpisode] = useState(0);
+  const carouselRef = useRef(null);
+  const seasons = ["1", "2", "3", "4", "5", "6"];
+  const episodes = {
+    1: [1, 2, 3, 4],
+    2: [5, 6, 7, 8],
+    3: [9, 10, 11, 12],
+    4: [13, 14, 15, 16],
+    5: [17, 18, 19, 20],
+    6: [21, 22, 23, 24],
+  };
+
+  const handleSelect = (index) => {
+    setDisplayEpisode(index);
+  };
+
   return (
     <Container>
       <Card className="p-2" bg="light" border="dark">
@@ -41,15 +49,19 @@ const JournalEntry = ({ show, openDefault = false }) => {
             <Card.Link>Go to details page</Card.Link>
           </Col>
         </Row>
-        <Button variant="primary" onClick={() => setOpen((open) => !open)}>
-          Toggle Episodes
+        <Button variant="secondary" onClick={() => setOpen((open) => !open)}>
+          {open ? "Hide Episodes" : "Show Episodes"}
         </Button>
         {open ? (
           <>
             <hr />
             <DropdownButton
-              title={`Season ${displaySeason}`}
-              onSelect={(value) => setDisplaySeason(value)}
+              title={`Season ${displaySeason} `}
+              onSelect={(value) => {
+                handleSelect(0);
+                setDisplaySeason(value);
+              }}
+              variant="secondary"
             >
               {seasons.map((season) => (
                 <Dropdown.Item key={season} eventKey={season}>
@@ -58,14 +70,27 @@ const JournalEntry = ({ show, openDefault = false }) => {
               ))}
             </DropdownButton>
             <Carousel
+              ref={carouselRef}
               variant="dark"
               interval={null}
               wrap={false}
-              className="my-3"
+              slide={false}
+              className="my-5"
+              indicators={false}
+              activeIndex={displayEpisode}
+              onSelect={handleSelect}
             >
-              <Carousel.Item>Episode 1</Carousel.Item>
-              <Carousel.Item>hello</Carousel.Item>
-              <Carousel.Item>hello</Carousel.Item>
+              {episodes[displaySeason]
+                ? episodes[displaySeason].map((episode) => (
+                    <Carousel.Item
+                      key={episode}
+                      as="h5"
+                      className="text-center"
+                    >
+                      Episode {episode}
+                    </Carousel.Item>
+                  ))
+                : null}
             </Carousel>
           </>
         ) : null}
