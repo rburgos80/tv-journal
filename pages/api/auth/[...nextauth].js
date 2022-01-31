f7a1223c42b7efbc761dd39966ac7e4a6e6b36f5
rburgos80 <rburgos80@gmail.com>
2022-01-31 16:28:17 -0500

    set up Email Provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,26 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
     }),
     // ...add more providers here
   ],
-  jwt: {
-    secret: process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
+  theme: {
+    colorScheme: "light",
   },
-  secret: process.env.NEXTAUTH_SECRET
-})
+});
