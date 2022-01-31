d0259df642ea2b5d026b8e103241c27d10227f8b
rburgos80 <rburgos80@gmail.com>
2022-01-31 18:33:55 -0500

    updated home page (still in progress)

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,38 @@ import Container from "react-bootstrap/Container";
 import ShowCard from "../components/ShowCard";
 
 export default function Home() {
-  const show = {
-    name: "The Sopranos",
-    image:
-      "https://static.tvmaze.com/uploads/images/medium_portrait/4/11341.jpg",
-    id: 527,
-  };
-
-  const shows = [show, show, show, show];
+  const shows = [
+    {
+      name: "The Sopranos",
+      image:
+        "https://static.tvmaze.com/uploads/images/medium_portrait/4/11341.jpg",
+      id: 527,
+    },
+    {
+      name: "Mr. Robot",
+      image:
+        "https://static.tvmaze.com/uploads/images/medium_portrait/211/528026.jpg",
+      id: 1871,
+    },
+    {
+      name: "Breaking Bad",
+      image:
+        "https://static.tvmaze.com/uploads/images/medium_portrait/0/2400.jpg",
+      id: 169,
+    },
+    {
+      name: "Better Call Saul",
+      image:
+        "https://static.tvmaze.com/uploads/images/medium_portrait/235/587815.jpg",
+      id: 618,
+    },
+    {
+      name: "Attack On Titan",
+      image:
+        "https://static.tvmaze.com/uploads/images/medium_portrait/311/779751.jpg",
+      id: 919,
+    },
+  ];
 
   return (
     <>
