39c8ea2f93da5ffbfc3c3426cb1ecd71b0aae5e0
rburgos80 <rburgos80@gmail.com>
2022-02-27 18:19:12 -0500

    changed profile dropdown title

--- components/Navbars.js
+++ components/Navbars.js
@@ -59,7 +59,7 @@ const Navbars = () => {
               )}
               {session && (
                 <>
-                  <NavDropdown title="Profile">
+                  <NavDropdown title="Account">
                     <NavDropdown.Header>
                       Signed in as: <strong>{session.user?.email}</strong>
                     </NavDropdown.Header>
