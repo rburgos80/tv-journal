2ffd2b5945b24aa2d94d74a37e2da753aeb45c1d
rburgos80 <rburgos80@gmail.com>
2022-03-08 18:11:55 -0500

    additional favicon links added

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,31 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/public/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/public/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/public/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/public/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/public/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#0275d8" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <Navbars />
         <Container fluid="sm" className="px-0 pb-md-4 my-md-4">
